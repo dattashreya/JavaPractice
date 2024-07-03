201afc8e215f0e602eb5f9590e3067dbb0336677
dattashreya <159568371+dattashreya@users.noreply.github.com>
2024-07-04 01:06:48 +0530

    OOP concepts

---
+++ EncapSetGet.java
@@ -0,0 +1,9 @@
+public class EncapSetGet {
+
+public static void main (String[] args) {
+    /* code */
+    Person Obj = new Person();
+    Obj.setName("shreya");
+    System.out.println(Obj.getName());
+    }    
+} 
