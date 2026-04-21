b624cb523d80c4dcd052843640551994a4f063f7
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-04-21 21:57:00 +0530

    StringConcat.java
    Implement string concatenation in StringConcat.java

---
+++ StringConcat.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    System.out.print("Enter s? ");
+	    Scanner o = new Scanner(System.in);
+        String s = o.next();
+        System.out.print("Enter d? ");
+        Scanner o1 = new Scanner(System.in);
+        String s1 = o1.next();
+	    System.out.println("String contact: "+s+" "+s1);
+	}
+}
