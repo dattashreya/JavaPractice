ed2c84ad4cf34d2895d6e828dddb3f6e81651ed6
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-04-21 20:36:19 +0530

    Boolean Check main method
    Added comment to clarify return value of comparison.

---
+++ BoolCheck.java
@@ -0,0 +1,7 @@
+package com.example.HelloWorld;
+public class HelloWorld extends BoolCheck {
+	public static void main(String[] args) {
+		int x =9; int y=8;
+		System.out.println(x>y);
+	}
+} // returns true
