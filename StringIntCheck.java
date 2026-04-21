123a10738d29757df7228fb3fa91fb42496d8c6b
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-04-21 20:54:25 +0530

    string int type checking program.java
    string int type checking program.java

---
+++ StringIntCheck.java
@@ -0,0 +1,7 @@
+package com.example.StringIntCheck;
+public class HelloWorld extends StringIntCheck {
+	public static void main(String[] args) {
+		int x1=9; String y1="9";
+		System.out.println(x1== Integer.parseInt(y1));
+	}
+}
