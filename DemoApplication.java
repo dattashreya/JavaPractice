2802618d9e5f5336093ee731afc4810786d369c3
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-04-22 16:51:16 +0530

    Spring initializer project with Maven
    along with Spring Web Dependency

--- DemoApplication.java
+++ DemoApplication.java
@@ -1,17 +1,13 @@
-package com.example.HelloWorld;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Scanner obj = new Scanner(System.in);
-		System.out.println("you name please?");
-		String n = obj.nextLine();
-		System.out.println("Name is-"+n);
 	}
+
 }
