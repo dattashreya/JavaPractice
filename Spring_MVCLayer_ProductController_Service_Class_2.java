8681682328802d65ef8db6da020b3a2b22cf3ad2
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-04-27 23:55:21 +0530

    Add ProductController, ProductService, and Product classes updating products
    Add ProductController, ProductService, and Product classes

---
+++ Spring_MVCLayer_ProductController_Service_Class_2.java
@@ -0,0 +1,102 @@
+package com.example.demo;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	ProductService s;
+	
+	@GetMapping("/products")
+	public List<Product> getProducts() {
+		return s.getProducts();
+	}
+	
+	@GetMapping("/products/{prodId}")
+	public Product getProductById(@PathVariable int prodId) {
+		return s.getProductById(prodId);
+	}
+	@PostMapping("/products")
+	public void addProduct(@RequestBody Product prod) {
+		s.addProduct(prod);
+	}
+}
+
+
+
+package com.example.demo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.Product;
+@Service
+public class ProductService {
+	List<Product> products = new ArrayList<>(Arrays.asList(
+			new Product(10,"Phone",1000),
+			new Product(11,"TV",2000)
+			));
+	public List<Product> getProducts() {
+	return products;
+	}
+	public Product getProductById(int prodId) {
+		return products.stream()
+				.filter(p -> p.getProdId() == prodId)
+//				.findFirst().get();
+				.findFirst().orElse(new Product(100,"None",0));
+	}
+	public void addProduct(Product prod) {
+		products.add(prod);
+	}
+}
+
+
+
+package com.example.demo;
+import org.springframework.stereotype.Component;
+@Component
+public class Product {
+	private int prodId;
+	private String prodName;
+	private int price;
+	public Product() {
+	}
+	public Product(int prodId, String prodName, int price) {
+		this.prodId = prodId;
+		this.prodName = prodName;
+		this.price = price;
+	}
+	public int getProdId() {
+		return prodId;
+	}
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [prodId=" + prodId + ", prodName=" + prodName + ", price=" + price + "]";
+	}
+	
+}
