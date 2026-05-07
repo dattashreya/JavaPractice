40f62b824d65c32cf47f1ec44d95d85d52dd138b
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-05-07 17:34:04 +0530

    Login Logout Auth Spring Controller with Session ID

---
+++ LoginAuthSessionIDSpringControl.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+public class Hello {
+
+	@GetMapping("/")
+	public String greet(HttpServletRequest request) {
+		return "hello Shreya,Welcome spring!" + request.getSession().getId();
+	}
+}
