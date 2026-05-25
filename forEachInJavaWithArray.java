c40b334a4864a3252671a4931db1cebd3992e7f8
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-05-25 22:55:37 +0530

    for Each In Java With Array

---
+++ forEachInJavaWithArray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    List<Integer> nums = Arrays.asList(4,2,3,1);
+	    nums.forEach(i -> System.out.println(i));
+	}
+}
